3271cdd3135f50d891632bc030bc3c3362b0b3e2
Alberto Lopez <alopez@llip.life>
2017-10-06 16:16:49 -0700

    Solve #4 Latest update

--- solutions/5.js
+++ solutions/5.js
@@ -2,11 +2,12 @@ const funct = ()=>{
   console.log('calling function');
 }
 
-const recursionFunct = (funct, num, f='') =>{
+const recursionFunct = (funct, num, fu='') =>{
   if(num === 0){
     return f;
   }
-  f = f + funct();
+  fun = fun + funct();
   return recursionFunct(funct, num-1);
 }
-recursionFunct(funct, 2);  
+//recursionFunct(funct, 2);  
+module.exports = recursionFunct;
