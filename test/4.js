8252e6703786198e928cd282afff65f139ad2686
Alberto Lopez <alopez@llip.life>
2017-10-06 16:22:01 -0700

    Solve #4

--- test/4.js
+++ test/4.js
@@ -1,17 +1,17 @@
 const solution = require('../solutions/4.js');
 
-const test = (a, n) =>{
-  if(solution(a, n) === `${a} ${a} ${a} ${a} ${a} `){
+const test = (a, n, tester) =>{
+  if(solution(a, n) === `${a} ${a} ${a} ${a} ${a} ` && tester === n){
     console.log('correct');
-  }else if(solution(a, n) === `${a} ${a} ${a} ${a} `){
+  }else if(solution(a, n) === `${a} ${a} ${a} ${a} ` && tester === n){
     console.log('correct');
-  }else if(solution(a, n) === `${a} ${a} ${a} `){
+  }else if(solution(a, n) === `${a} ${a} ${a} ` && tester === n){
     console.log('correct');
   }else{
     console.log('incorrect');
   }
 }
-test('Apple', 5)
-test('Apple', 4);
-test('Apple', 3);
+test('Apple', 5, 5)
+test('Apple', 4, 5);
+test('Apple', 3, 3);
 test('Apple', 10);
