2a93151b33a8e5fe6641563fa7e70b9eface61df
Alberto Lopez <alopez@llip.life>
2017-10-06 12:44:55 -0700

    Solve #3

--- solutions/3.js
+++ solutions/3.js
@@ -1,12 +1,12 @@
-const sayHelloTimes = (string, times, str='', count=0, i=0) => {
+const sayHelloTimes = (string, times, str='', i=0) => {
   if(i === times){
-    return count;
+    return str;
   }
   if(i != times){
-    count = count + i;
     str = str + string + ' ';
   }
-  return sayHelloTimes(string, times, str, count, i+1);
+  return sayHelloTimes(string, times, str, i+1);
 
 };
+console.log(sayHelloTimes('hello', 3));
 module.exports = sayHelloTimes;
