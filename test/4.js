24ced01a561bc2be230568a74c23c7685b894157
Alberto Lopez <alopez@llip.life>
2017-10-06 15:16:22 -0700

    Solve #4

--- test/4.js
+++ test/4.js
@@ -3,11 +3,15 @@ const solution = require('../solutions/4.js');
 const test = (a, n) =>{
   if(solution(a, n) === `${a} ${a} ${a} ${a} ${a} `){
     console.log('correct');
-    console.log(solution(a,n));
+  }else if(solution(a, n) === `${a} ${a} ${a} ${a} `){
+    console.log('correct');
+  }else if(solution(a, n) === `${a} ${a} ${a} `){
+    console.log('correct');
   }else{
     console.log('incorrect');
   }
 }
 test('Apple', 5)
 test('Apple', 4);
+test('Apple', 3);
 test('Apple', 10);
