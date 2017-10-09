d7445da1d57c63e748ccbbf03be700b0da0b65ea
Alberto Lopez <alopez@llip.life>
2017-10-09 16:42:39 -0700

    Solve #5 Update

--- test/5.js
+++ test/5.js
@@ -1,32 +1,20 @@
 const solution = require('../solutions/5.js');
-
 let i = 0;
 const funct = ()=>{
   i = i + 1 ;
 }
 
-solution(funct, 5);
-
-if(i === 5){
-  console.log('correct');
-}else{
-  console.log('incorrect');
+const tester = (call, tes)=>{
+  solution(funct, call);
+  if(tes === i){
+    console.log('correct');
+  }else{
+    console.log('incorrect');
+  }
+  i=0;
 }
 
-i=0;
-solution(funct, 10);
+tester(8,6);
+tester(10, 10);
+tester(3,3);
 
-if(i === 10){
-  console.log('correct');
-}else{
-  console.log('incorrect');
-}
-
-i=0;
-solution(funct, 15);
-
-if(i === 15){
-  console.log('correct');
-}else{
-  console.log('incorrect');
-}
