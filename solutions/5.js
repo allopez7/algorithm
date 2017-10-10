b856225d28184a9c4f159620cc49afec7c315af1
Alberto Lopez <alopez@llip.life>
2017-10-09 18:21:18 -0700

    Solve cinco

--- solutions/5.js
+++ solutions/5.js
@@ -1,7 +1,7 @@
 const recursionFunct = (funct, num) =>{
   if(num > 0){
   funct();
-  return recursionFunct(funct, num-1);
+  recursionFunct(funct, num-1);
   }
 }
 module.exports = recursionFunct;
