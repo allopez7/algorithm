a3079d7b4862446238fcfbd5385a8a0cf9cb7d18
Alberto Lopez <alopez@llip.life>
2017-10-11 14:05:31 -0700

    Solve #7

--- solutions/7.js
+++ solutions/7.js
@@ -2,10 +2,8 @@ const getDivisors = (number, i=1, max=0) =>{
   if(i === number){
     return max;
   }
-  if(number%i === 0){
-    if(i> max){
+  if(number%i === 0 && i>max){
       max = i;
-    }
   }
   return getDivisors(number, i+1, max);
 }
