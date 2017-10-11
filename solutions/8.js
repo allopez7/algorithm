1ce1ad9541952511c4c886b3fe79a46b3d2abf0a
Alberto Lopez <alopez@llip.life>
2017-10-11 14:35:35 -0700

    Solve #8

--- solutions/8.js
+++ solutions/8.js
@@ -1,17 +1,26 @@
-const isPrime = (number, i=1)=>{
-  if(number === 0 || number === 1 || number%2 === 0){
-    console.log(number, i);
+const isPrime = (number, i=2)=>{
+  if(number === 0 || number === 1){
     return false;
   } 
-    if((number%i != 0 && number%i != 1) || number === 2){
-      console.log(number, i);
+  if(number%i === 0){
+    if(number%2 === 0){
+      return true;
+    }
+    return false;
+  }
+    if(number%i != 0 && number%i != 1){
       return true;
     }
 
   return isPrime(number, i+1);
 }
-console.log(isPrime(8));
-console.log(isPrime(7));
+console.log(isPrime(1));
 console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
 console.log(isPrime(5));
-console.log(isPrime(11));
+console.log(isPrime(6));
+console.log(isPrime(7));
+console.log(isPrime(8));
+console.log(isPrime(9));
+console.log(isPrime(10));
