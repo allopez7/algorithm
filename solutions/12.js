36a5013205040d0f4e8194260c0338b8e2783d3d
Alberto Lopez <alopez@llip.life>
2017-10-12 15:05:37 -0700

    Solve #12

--- solutions/12.js
+++ solutions/12.js
@@ -1,16 +1,7 @@
-const factorialFunct =  (number, factorial=1, i=1)=>{
-  if(number === 0){
+const factorialFunct = (num)=>{
+  if(num <= 1){
     return 1;
   }
-  if(number < 0){
-    return;
-  }
-  if(number < i){
-    return factorial*number;
-  }
-  if(i != number){
-    factorial = factorial*i;
-  }
-  return factorialFunct(number, factorial, i+1);
+  return num*factorialFunct(num-1);
 }
 module.exports = factorialFunct;
