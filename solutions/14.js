f1580022973d867f28215d9905695b7f3559ece6
Alberto Lopez <alopez@llip.life>
2017-10-16 16:46:38 -0700

    Solve #14

--- solutions/14.js
+++ solutions/14.js
@@ -1,16 +1,15 @@
-
 const check = (num, newArr=[], i=2, j=2)=>{
-  if(i > num){
+  if(j > num){
     return newArr;
   }
   if(i === j){
     newArr.push(i);
-    return check(num, newArr, i+1, j+1);
+    return check(num, newArr, i+1, j);
   }
-  if(num%i === 0){
-    return check(num, newArr, i, j+1);
+  if(j%i === 0){
+    return check(num, newArr, i);
   }
-  return check(num, newArr, i+1, j+1);
+  return check(num, newArr, i, j+1);
 }
 console.log(check(100));
 //module.exports = isPrime;
