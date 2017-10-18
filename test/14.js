18c12bef00aca7f4615961849d6d3edf150c5d7d
Alberto Lopez <alopez@llip.life>
2017-10-17 18:44:16 -0700

    Solve prime numbers from 1-x

--- test/14.js
+++ test/14.js
@@ -1,18 +1,17 @@
 const solution = require('../solutions/14.js');
 
-const tester = (upTo, i=0)=>{
-  if(i > upTo){
-    return;
+const tester = (upTo, result, i=0)=>{
+  if(i >= upTo){
+    console.log('correct');
+    return true;
   }
-  console.log(solution(upTo)[i]);
-    /*
-  if(solution(upTo) === i){
-    console.log(i+' correct');
-  }else{
-    console.log(i+' incorrect');
+  if(solution(upTo)[i] != result[i]){
+    console.log('incorrect');
+    return false;
   }
-  */
-  return tester(upTo, i+1); 
+  return tester(upTo, result, i+1); 
 }
 
-tester(17);
+tester(7, [2,3,5,7]);
+tester(17, [2,3,5,7,9,11,13,17]);
+tester(13, [2,3,5,7,11,13]);
