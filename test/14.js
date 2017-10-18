e8cdd9c6dce79aa3c7a84dfed58d72c0601a06ec
Alberto Lopez <alopez@llip.life>
2017-10-17 21:12:47 -0700

    Primes from 1-x

--- test/14.js
+++ test/14.js
@@ -1,15 +1,21 @@
 const solution = require('../solutions/14.js');
 
-const tester = (upTo, result, i=0)=>{
-  if(i >= upTo){
-    console.log('correct');
+const helper = (ar, ar1, i=0)=>{
+  if(i === ar.length){
     return true;
   }
-  if(solution(upTo)[i] != result[i]){
-    console.log('incorrect');
+  if(ar[i] != ar1[i]){
     return false;
   }
-  return tester(upTo, result, i+1); 
+  return helper(ar, ar1, i+1);
+}
+const tester = (upTo, result)=>{
+  const newArr = solution(upTo);
+  if(helper(newArr,result)){
+    console.log('prime');
+  }else{
+    console.log('not prime');
+  }
 }
 
 tester(7, [2,3,5,7]);
