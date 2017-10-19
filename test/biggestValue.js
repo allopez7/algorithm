e7dab84d7b0effcaeacfff780061595c94f00f8e
Alberto Lopez <alopez@llip.life>
2017-10-18 18:06:56 -0700

    Objects: Lesson1

--- test/biggestValue.js
+++ test/biggestValue.js
@@ -1,12 +1,12 @@
 const solution = require('../solutions/biggestValue.js');
 
 const tester = (object, result) =>{
-  if(solution(obj) === result){
+  if(solution(object) === result){
     console.log('correct');
   }else{
     console.log('incorrect');
   }
 }
-tester({one: 1, two: 2, three: 3});
-tester({"key": 3, 'value': 40, 'word': 50});
-tester({1:'Hi',2:3,4:"hey"}
+tester({one: 1, two: 2, three: 3}, 3);
+tester({"key": 3, 'value': 40, 'word': 50}, 40);
+tester({1:'Hi',2:3,4:"hey"}, 3);
